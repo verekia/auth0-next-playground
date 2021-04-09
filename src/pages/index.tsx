fa407822a15e801006c891912dfa9c621756d63b
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-04-09 18:52:43 +0700

    Auth0 integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-const IndexPage = () => <div>Index</div>
+import { useUser } from '@auth0/nextjs-auth0'
 
-export default IndexPage
+export default function IndexPage() {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  }
+
+  return <a href="/api/auth/login">Login</a>
+}
